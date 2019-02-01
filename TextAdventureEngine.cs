37831f0915131590ea91e18bf2054e83cba28078
Tactless2004 <ec2-user@ip-172-31-46-70.us-east-2.compute.internal>
2019-02-01 20:21:11 +0000

    Beta Combat System, Monster bug fixes

--- TextAdventureEngine.cs
+++ TextAdventureEngine.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Threading;
 
 public class TextAdventureEngine 
 {
     //Temporary Main to Compile Delete Later
     public static void Main(string[] args)
     {
+        
+         Monster Goblin = new Monster();
+            Goblin.HP = 5;
+            Goblin.AC = 7;
+            Goblin.Name = "Goblin";
+            Goblin.MonsterMove1.Name = "Slash";
+            Goblin.MonsterMove1.MaxDamage = 4;
+        
         Character Player = new Character();
         Player = CharacterFunctions.CharCreator();
+        
+        Console.WriteLine("Bloise:Im going to run you through some example combat to get you used to the system");
+        
+        CombatManager.StartCombat(Player, Goblin);
     }
     
     //Defines Class Type
@@ -63,25 +76,20 @@ public class TextAdventureEngine
             Character OutputCharacter = new Character();
             OutputCharacter = InputCharacter;
             
-            Console.WriteLine(OutputCharacter.CharClass);
             //Assigns stats if InputCharacter is a Fighter
             if(OutputCharacter.CharClass == Classes.Fighter)
             {
                 OutputCharacter.Move1.Name = "Sword Slash";
                 OutputCharacter.Move1.MaxDamage = 8;
-                OutputCharacter.Move1.ToHit = 75;
                 
                 OutputCharacter.Move2.Name = "Shield Bash";
                 OutputCharacter.Move2.MaxDamage = 4;
-                OutputCharacter.Move2.ToHit = 60;
                 
                 OutputCharacter.Move3.Name = "Rage";
                 OutputCharacter.Move3.MaxDamage = 1000;
-                OutputCharacter.Move3.ToHit = 20;
                 
                 OutputCharacter.Move4.Name = "Intimidate";
                 OutputCharacter.Move4.MaxDamage = 0;
-                OutputCharacter.Move4.ToHit = 40;
                 
                 OutputCharacter.QuestionText = "1:ShordSlash, 2: ShieldBash, 3:Rage 4: Intimidate";
                 
@@ -94,19 +102,15 @@ public class TextAdventureEngine
             {
                 OutputCharacter.Move1.Name = "Slice";
                 OutputCharacter.Move1.MaxDamage = 8;
-                OutputCharacter.Move1.ToHit = 75;
-                
+
                 OutputCharacter.Move2.Name = "Poison Throw";
                 OutputCharacter.Move2.MaxDamage = 4;
-                OutputCharacter.Move2.ToHit = 80;
                 
                 OutputCharacter.Move3.Name = "Back Stab";
                 OutputCharacter.Move3.MaxDamage = 1000;
-                OutputCharacter.Move3.ToHit = 20;
                 
                 OutputCharacter.Move4.Name = "Stealth Disengage";
                 OutputCharacter.Move4.MaxDamage = 0;
-                OutputCharacter.Move4.ToHit = 40;
                 
                 
                 
@@ -121,19 +125,15 @@ public class TextAdventureEngine
             {
                 OutputCharacter.Move1.Name = "Fire Ball";
                 OutputCharacter.Move1.MaxDamage = 10;
-                OutputCharacter.Move1.ToHit = 50;
                 
                 OutputCharacter.Move2.Name = "Frost Bolt";
                 OutputCharacter.Move2.MaxDamage = 8;
-                OutputCharacter.Move2.ToHit = 60;
                 
                 OutputCharacter.Move3.Name = "Unholy Devastation";
                 OutputCharacter.Move3.MaxDamage = 1000;
-                OutputCharacter.Move3.ToHit = 20;
                 
                 OutputCharacter.Move4.Name = "Teleport";
                 OutputCharacter.Move4.MaxDamage = 0;
-                OutputCharacter.Move4.ToHit = 40;
                 
                 OutputCharacter.QuestionText = "1:FireBall, 2:FrostBolt, 3:UnholyDevastation 4:Teleport";
                 
@@ -146,19 +146,15 @@ public class TextAdventureEngine
             {
                 OutputCharacter.Move1.Name = "Bow Shot";
                 OutputCharacter.Move1.MaxDamage = 8;
-                OutputCharacter.Move1.ToHit = 50;
                 
                 OutputCharacter.Move2.Name = "Entangle";
                 OutputCharacter.Move2.MaxDamage = 4;
-                OutputCharacter.Move2.ToHit = 60;
                 
                 OutputCharacter.Move3.Name = "Tree Curse";
                 OutputCharacter.Move3.MaxDamage = 1000;
-                OutputCharacter.Move3.ToHit = 20;
                 
                 OutputCharacter.Move4.Name = "Stealth Escape";
                 OutputCharacter.Move4.MaxDamage = 0;
-                OutputCharacter.Move4.ToHit = 40;
                 
                 OutputCharacter.QuestionText = "1:BowShot, 2:Entangle, 3:TreeCurse 4:StealthEscape";
                 
@@ -179,23 +175,22 @@ public class TextAdventureEngine
     {
         public int HP;
         public int AC;
-        public int Name;
-        public string AttackName;
-        public string AttackToHit;
-        public string AttackDamageMax;
+        public string Name;
+        
+        public Move MonsterMove1 = new Move();
+           
     }
     
     public class Move 
     {
         public string Name;
-        public int ToHit;
         public int MaxDamage;
     }
     //Contains Monster List 
     //Later Convert to a Mark Up Language
-    public class MonsterList
+    public static class DefaultMonster
     {
-      
+        //Nested in a function because c# is d
     }
     
     //Combat Manager contains everything related to combat
@@ -203,30 +198,88 @@ public class TextAdventureEngine
     {
         public static string StartCombat(Character character, Monster monster)
         {
-            char[] AttackDelimiter = {' '};
             bool InCombat = true;
             int MoveChosen;
+            //Combat Variables For Monster
+                int MonsterAC = monster.AC;
+                int MonsterHP = monster.HP;
+                string MonsterName = monster.Name;
+                
+            //Used to track current move data    
+                Move CurrentMove = new Move();
             
+            //Combat Variables For Monster
+                int PlayerAC =  character.AC;
+                int PlayerHP = character.HP;
+                string PlayerName = character.Name;
+                
+            //Used as a RNG
+                Random RNG = new Random();
+                
+            //intergers used to for AC
+                int ToHit;
+                int CalculatedDamage;
+            //Combat Runner 
             while(InCombat)
             {
-                Console.WriteLine("HP: " + character.HP);
-                Console.WriteLine("Monster HP: " + monster.HP);
+                Console.WriteLine("HP: " + PlayerHP);
+                Console.WriteLine("-------------------- ");
+                Console.WriteLine("Monster HP: " + MonsterHP);
+                Console.WriteLine(" ");
                 Console.WriteLine("Choose Move: " + character.QuestionText);
                 
                 MoveChosen = Int32.Parse(Console.ReadLine());
                 
-                //Do Character Attack
+                //Sets Current Move equal to the move that goes with the number chosen
+                switch(MoveChosen)
+                {
+                    case 1:
+                        CurrentMove = character.Move1;
+                        break;
+                    case 2:
+                        CurrentMove = character.Move2;
+                        break;
+                    case 3:
+                        CurrentMove = character.Move3;
+                        break;
+                    case 4:
+                        CurrentMove = character.Move4;
+                        break;
+                }
                 
-                //Check if Enemey Dead
+                //Determines If hit
+                ToHit = RNG.Next(1,20);
                 
-                //Get Enemy Attack
+                //Applys Damage if Hit
+                if(ToHit>=MonsterAC)
+                {
+                    CalculatedDamage = RNG.Next(1, CurrentMove.MaxDamage);
+                    MonsterHP-=CalculatedDamage;
+                }
                 
-                //Do Enemy Attack
+                if(MonsterHP<=0)
+                {
+                    InCombat = false;
+                    return PlayerName +" has vanquished " + MonsterName;
+                }
                 
-                //Check if Enemy Dead
+                CurrentMove = monster.MonsterMove1;
                 
-                //If one is dead return death message
-                return "Done";
+                //Checks if Enemy Hit Player
+                ToHit = RNG.Next(1,20);
+                
+                //If Enemy hit player do damage
+                if(ToHit>=PlayerAC)
+                {
+                   CalculatedDamage = RNG.Next(1, CurrentMove.MaxDamage);
+                   PlayerHP-=CalculatedDamage; 
+                }
+                
+                if(PlayerHP<=0)
+                {
+                    InCombat = false;
+                    return MonsterName +" has vanquished " + PlayerName;
+                }
             }
             return "Done";
         }
